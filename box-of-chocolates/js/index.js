1579a2645a2e79a1a294f679467ebef2208faa67
Tgrafix Design Studio <tgrafix.design@yahoo.com>
2025-10-24 15:51:27 +0800

    update index with info gif
    update index with info gif

--- box-of-chocolates/js/index.js
+++ box-of-chocolates/js/index.js
@@ -297,7 +297,7 @@
     var iconWrapper = document.createElement('div');
     iconWrapper.classList.add('info-hotspot-icon-wrapper');
     var icon = document.createElement('img');
-    icon.src = 'img/info.png';
+    icon.src = 'img/info.gif';
     icon.classList.add('info-hotspot-icon');
     iconWrapper.appendChild(icon);
 
